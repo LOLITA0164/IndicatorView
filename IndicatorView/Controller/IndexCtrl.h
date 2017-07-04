1d1347d1779adc58459957d94aa29ca0293b080f
LOLITA <476512340@qq.com>
2017-07-04 21:32:41 +0800

    dd
    dd

--- IndicatorView/Controller/IndexCtrl.h
+++ IndicatorView/Controller/IndexCtrl.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface IndexCtrl : UICollectionViewController
+@interface IndexCtrl : UIViewController
 
 @end
--- IndicatorView/Controller/IndexCtrl.m
+++ IndicatorView/Controller/IndexCtrl.m
@@ -9,109 +9,57 @@
 #import "IndexCtrl.h"
 #import "IndicatorView.h"
 
-@interface IndexCtrl ()<UICollectionViewDelegateFlowLayout>
+@interface IndexCtrl ()
+
+
 
 @end
 
 @implementation IndexCtrl
 
-static NSString * const reuseIdentifier = @"Cell";
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    // Uncomment the following line to preserve selection between presentations
-    // self.clearsSelectionOnViewWillAppear = NO;
     
-    // Register cell classes
-    [self.collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:reuseIdentifier];
-    
-    // Do any additional setup after loading the view.
+    [self initUI];
 }
+
+
+
+-(void)initUI{
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    self.title = @"IndicatorView";
+    
+    NSInteger num = 1;
+    
+    CGFloat x = [UIScreen mainScreen].bounds.size.width/3.0;
+    CGFloat y = [UIScreen mainScreen].bounds.size.height/5.0;
+    
+    for (int i=0; i<num; i++) {
+        
+        NSInteger row = i % 2;
+        NSInteger line = ceilf(i/2.0);
+        
+        IndicatorView *indicator = [[IndicatorView alloc] initWithType:i];
+        indicator.center = CGPointMake(x*(row+1), y*line);
+        [indicator startAnimating];
+        
+        [self.view addSubview:indicator];
+        
+    }
+    
+    
+}
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
-
-#pragma mark <UICollectionViewDataSource>
-
-- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
-    return 1;
-}
-
-
-- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
-    return 1;
-}
-
-- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
-    
-    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:reuseIdentifier forIndexPath:indexPath];
-    
-    //用Integer代替枚举
-    
-    for (UIView *view in cell.contentView.subviews) {
-        [view removeFromSuperview];
-    }
-    
-//    IndicatorView *indicator = [[IndicatorView alloc] initWithType:indexPath.row];
-//    
-//    indicator.center = cell.contentView.center;
-//    [cell.contentView addSubview:indicator];
-//    [indicator startAnimating];
-    
-
-    // Configure the cell
-    
-    return cell;
-}
-
-#pragma mark - <UICollectionViewDelegateFlowLayout>
-
-- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath{
-    return CGSizeMake(100, 100);
-}
-
-#pragma mark <UICollectionViewDelegate>
-
-/*
-// Uncomment this method to specify if the specified item should be highlighted during tracking
-- (BOOL)collectionView:(UICollectionView *)collectionView shouldHighlightItemAtIndexPath:(NSIndexPath *)indexPath {
-	return YES;
-}
-*/
-
-/*
-// Uncomment this method to specify if the specified item should be selected
-- (BOOL)collectionView:(UICollectionView *)collectionView shouldSelectItemAtIndexPath:(NSIndexPath *)indexPath {
-    return YES;
-}
-*/
-
-/*
-// Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-- (BOOL)collectionView:(UICollectionView *)collectionView shouldShowMenuForItemAtIndexPath:(NSIndexPath *)indexPath {
-	return NO;
-}
-
-- (BOOL)collectionView:(UICollectionView *)collectionView canPerformAction:(SEL)action forItemAtIndexPath:(NSIndexPath *)indexPath withSender:(id)sender {
-	return NO;
-}
-
-- (void)collectionView:(UICollectionView *)collectionView performAction:(SEL)action forItemAtIndexPath:(NSIndexPath *)indexPath withSender:(id)sender {
-	
-}
-*/
 
 @end
